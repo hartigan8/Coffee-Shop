1eb801b655eac78fabe03896119eb547f3bb0bd7
mcany <83024513+hartigan8@users.noreply.github.com>
2022-10-26 01:34:09 +0300

    Initial Commit

---
+++ CinAction/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CinAction
+//
+//  Created by Mustafa Can YILMAZ on 26.10.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
